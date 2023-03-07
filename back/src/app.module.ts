3526bd5b32473625dc553cd8dec551d6cf048833
odelalande <delalande.olivier@gmail.com>
2023-03-07 21:39:57 +0100

    Remove postgres concerns

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,19 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({isGlobal: true}),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: process.env.DATABASE_URL,
-      autoLoadEntities:true,
-      synchronize: true
-    })
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
